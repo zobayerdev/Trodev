c34710ff6ddfdc1d3f9c7e897f79f98c2580e391
rubayet <rubayetislam16@gmail.com>
2024-01-28 00:15:23 +0600

    email

--- resources/views/eventmail.blade.php
+++ resources/views/eventmail.blade.php
@@ -451,20 +451,20 @@
 
 <body width="100%" bgcolor="#F1F1F1" style="margin: 0; mso-line-height-rule: exactly;">
 <center style="width: 100%; background: #F1F1F1; text-align: left;">
-    <div style="display:none;font-size:1px;line-height:1px;max-height:0px;max-width:0px;opacity:0;overflow:hidden;mso-hide:all;font-family: sans-serif;"> (Optional) This text will appear in the inbox preview, but not the email body. </div>
+    <div style="display:none;font-size:1px;line-height:1px;max-height:0px;max-width:0px;opacity:0;overflow:hidden;mso-hide:all;font-family: sans-serif;"> Welcome to our website  Mr. {{$userId['name']}} </div>
     <div style="max-width: 680px; margin: auto;" class="email-container">
         <table role="presentation" cellspacing="0" cellpadding="0" border="0" align="center" width="100%" style="max-width: 680px;" class="email-container">
             <tr>
                 <td bgcolor="#333333">
                     <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
                         <tr>
-                            <td style="padding: 30px 40px 30px 40px; text-align: center;"> <span style="color:#fff; font-size: 30px">BBBootstrap.com</span> </td>
+                            <td style="padding: 30px 40px 30px 40px; text-align: center;"> <span style="color:#fff; font-size: 30px">Trodev</span> </td>
                         </tr>
                     </table>
                 </td>
             </tr>
             <tr>
-                <td background="https://i.imgur.com/eZflxhL.jpg" bgcolor="#222222" align="center" valign="top" style="text-align: center; background-position: center center !important; background-size: cover !important;">
+                <td background="https://drive.google.com/file/d/19wgBVMGAlOvgh-cVvJi0gQD0nqvK3Iks/view?usp=sharing" bgcolor="#222222" align="center" valign="top" style="text-align: center; background-position: center center !important; background-size: cover !important;">
                     <div>
                         <table role="presentation" border="0" cellpadding="0" cellspacing="0" align="center" width="100%" style="max-width:500px; margin: auto;">
                             <tr>
@@ -488,7 +488,7 @@
                                                 <center>
                                                     <table role="presentation" align="center" cellspacing="0" cellpadding="0" border="0" class="center-on-narrow" style="text-align: center;">
                                                         <tr>
-                                                            <td style="border-radius: 50px; background: #26a4d3; text-align: center;" class="button-td"> <a href="http://www.google.com" style="background: #26a4d3; border: 15px solid #26a4d3; font-family: 'Montserrat', sans-serif; font-size: 14px; line-height: 1.1; text-align: center; text-decoration: none; display: block; border-radius: 50px; font-weight: bold;" class="button-a"> <span style="color:#ffffff;" class="button-link">&nbsp;&nbsp;&nbsp;&nbsp;ACCESS ACCOUNT&nbsp;&nbsp;&nbsp;&nbsp;</span> </a> </td>
+                                                            <td style="border-radius: 50px; background: #26a4d3; text-align: center;" class="button-td"> <a href="{{route('homepage',['id'=>$userId['id']])}}" style="background: #26a4d3; border: 15px solid #26a4d3; font-family: 'Montserrat', sans-serif; font-size: 14px; line-height: 1.1; text-align: center; text-decoration: none; display: block; border-radius: 50px; font-weight: bold;" class="button-a"> <span style="color:#ffffff;" class="button-link">&nbsp;&nbsp;&nbsp;&nbsp;ACCESS ACCOUNT&nbsp;&nbsp;&nbsp;&nbsp;</span> </a> </td>
                                                         </tr>
                                                     </table>
                                                 </center>
@@ -544,7 +544,7 @@
                                 <!-- Button : BEGIN -->
                                 <table role="presentation" align="center" cellspacing="0" cellpadding="0" border="0" class="center-on-narrow">
                                     <tr>
-                                        <td style="border-radius: 50px; background: #ffffff; text-align: center;" class="button-td"> <a href="http://www.google.com" style="background: #ffffff; border: 15px solid #ffffff; font-family: 'Montserrat', sans-serif; font-size: 14px; line-height: 1.1; text-align: center; text-decoration: none; display: block; border-radius: 50px; font-weight: bold;" class="button-a"> <span style="color:#26a4d3;" class="button-link">&nbsp;&nbsp;&nbsp;&nbsp;GIVE FEEDBACK&nbsp;&nbsp;&nbsp;&nbsp;</span> </a> </td>
+                                        <td style="border-radius: 50px; background: #ffffff; text-align: center;" class="button-td"> <a href="mailto: info@trodev.com" style="background: #ffffff; border: 15px solid #ffffff; font-family: 'Montserrat', sans-serif; font-size: 14px; line-height: 1.1; text-align: center; text-decoration: none; display: block; border-radius: 50px; font-weight: bold;" class="button-a"> <span style="color:#26a4d3;" class="button-link">&nbsp;&nbsp;&nbsp;&nbsp;GIVE FEEDBACK&nbsp;&nbsp;&nbsp;&nbsp;</span> </a> </td>
                                     </tr>
                                 </table>
                             </td>
@@ -560,17 +560,17 @@
                         </tr>
                         <tr>
                             <td align="center" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 24px; padding: 5px 0 10px 0;">
-                                <p style="font-size: 14px; font-weight: 800; line-height: 18px; color: #333333;"> BBBootstrap.com<br> Street, NJ,USA </p>
+                                <p style="font-size: 14px; font-weight: 800; line-height: 18px; color: #333333;"> Trodev<br> H#34/4, Primary School Road, Islampur, Dattapara, House-Building, Tongi, Gazipur </p>
                             </td>
                         </tr>
                         <tr>
                             <td style="padding: 0px 40px 10px 40px; font-family: sans-serif; font-size: 12px; line-height: 18px; color: #666666; text-align: center; font-weight:normal;">
-                                <p style="margin: 0;">This email was sent to you from contact@bbbootstrap.com</p>
+                                <p style="margin: 0;">This email was sent to you from info@trodev.com</p>
                             </td>
                         </tr>
                         <tr>
                             <td style="padding: 0px 40px 40px 40px; font-family: sans-serif; font-size: 12px; line-height: 18px; color: #666666; text-align: center; font-weight:normal;">
-                                <p style="margin: 0;">Copyright &copy; 2018-2019 <b>BBBootstrap.com</b>, All Rights Reserved.</p>
+                                <p style="margin: 0;">Copyright &copy; <script>document.write(new Date().getFullYear());</script> <b>Trodev</b>, All Rights Reserved.</p>
                             </td>
                         </tr>
                     </table>
